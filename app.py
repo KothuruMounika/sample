214d67a498d1e80c283b364241b1b945b2ce7fa7
KothuruMounika <kothurumounikareddy9977@gmail.com>
2025-08-02 12:17:18 +0530

    third part

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/")
+#create templates folder=>index.html
 def index():
-    return "Welcome to MRECW"
+    return render_template("index.html")
 @app.route("/home")
 def home():
     return "home page"
+@app.route("/index1")
+def index1():
+    return "index1 page"
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=10000)
